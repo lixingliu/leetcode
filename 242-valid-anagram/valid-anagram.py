6a7188e5c9e4cdad8e623bc209fa4e0c135fb420
lixingliu <89395611+lixingliu@users.noreply.github.com>
2025-09-08 15:53:33 -0400

    Time: 14 ms (45.93%) | Memory: 17.8 MB (86.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,18 +3,19 @@ class Solution:
         if (len(s) != len(t)):
             return False
         dict_s = dict()
-        dict_t = dict()
         for char in s:
             if char in dict_s:
                 dict_s[char] += 1
             else:
                 dict_s[char] = 1
         for char in t:
-            if char in dict_t:
-                dict_t[char] += 1
+            if char in dict_s:
+                dict_s[char] -= 1
+                if dict_s[char] == 0:
+                    dict_s.pop(char)
             else:
-                dict_t[char] = 1
-
-        return dict_s == dict_t
+                return False
+        print(dict_s)
+        return True if len(dict_s) == 0 else False
 
             
