045afb6d75752fa801ac8e20edc732996c1ff3ae
lixingliu <89395611+lixingliu@users.noreply.github.com>
2025-09-10 14:00:05 -0400

    Time: 3 ms (12.10%) | Memory: 18 MB (17.74%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        #edge cases if list1 or list2 is Empty
+        if list1 == None and list2 == None:
+            return list1
+        elif list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+        
+        #logic to merge two lists
+        list3 = ListNode()  #initialize linkedlist to return
+        list3Head = list3   #store the head to return
+        print("initial", list3)
+        #helper function to recursely search through linked list
+        def helper(list1, list2, list3):
+            if list1 == None and list2 == None:
+                return
+
+
+            if list1 == None and list2 != None:                
+                list3.val = list2.val
+                list2 = list2.next
+            if list1 != None and list2 == None:
+                list3.val = list1.val
+                list1 = list1.next
+            if list1 != None and list2 != None:
+                if list1.val <= list2.val:
+                    list3.val = list1.val
+                    list1 = list1.next
+                else:
+                    list3.val = list2.val
+                    list2 = list2.next
+            if list1 != None or list2 != None:
+                list3.next = ListNode()
+                list3 = list3.next
+                
+            helper(list1, list2, list3)
+                
+
+
+            
+        helper(list1, list2, list3)
+        return list3Head
