be3be2d1b2b0b42bf9344c6384585098bc52e517
lixingliu <89395611+lixingliu@users.noreply.github.com>
2025-09-10 15:03:46 -0400

    Time: 7 ms (44.98%) | Memory: 18 MB (74.36%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -7,7 +7,7 @@ class Solution:
         n = len(list1)
         m = len(list2)
         big = max(n, m)
-        extra = [0] * big
+        carry = 0
         if n > m:
             for _ in range(n-m):
                 list2.insert(0, "0")
@@ -15,13 +15,14 @@ class Solution:
             for _ in range(m-n):
                 list1.insert(0, "0")
         for i in range(big - 1, -1, -1):
-            sum = (ord(list1[i]) - zero) + (ord(list2[i]) - zero) + extra[i]
+            sum = (ord(list1[i]) - zero) + (ord(list2[i]) - zero) + carry
             if i == 0:
                 ans = str(sum) + ans
                 break
             if sum > 9:
                 ans = str(sum - 10) + ans 
-                extra[i-1] = 1
+                carry= 1
             else:
                 ans = str(sum) + ans
+                carry = 0
         return ans  
