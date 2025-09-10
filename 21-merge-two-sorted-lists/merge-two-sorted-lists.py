d791b1e369a69e9713e57ac9c80148c255bcd525
lixingliu <89395611+lixingliu@users.noreply.github.com>
2025-09-10 14:17:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -16,10 +16,9 @@ class Solution:
         list3 = ListNode()  #initialize linkedlist to return
         list3Head = list3   #store the head to return
         #helper function to recursely search through linked list
-        def helper(list1, list2, list3):
-            if list1 == None and list2 == None:
-                return
-            if list1 == None and list2 != None:                
+
+        while list1 or list2:
+            if list1 == None and list2 != None:             
                 list3.val = list2.val
                 list2 = list2.next
             if list1 != None and list2 == None:
@@ -35,6 +34,4 @@ class Solution:
             if list1 != None or list2 != None:
                 list3.next = ListNode()
                 list3 = list3.next
-            helper(list1, list2, list3)
-        helper(list1, list2, list3)
         return list3Head
